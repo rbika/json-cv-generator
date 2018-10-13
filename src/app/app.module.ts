2dff6916d748b95435820b1d73d0ff00cd7882ce
Rafael Bicalho <rbmbika@gmail.com>
2018-10-12 23:26:12 -0300

    Move header to a separete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CvModule } from './cv/cv.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
     ],
     imports: [
         BrowserModule,
