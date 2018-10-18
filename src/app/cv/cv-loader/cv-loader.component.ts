fe32ac958e6edfa343f9dd7fd208664f312bfd5b
Rafael Bicalho <rbmbika@gmail.com>
2018-10-18 02:01:57 -0300

    Handle file upload errors

--- src/app/cv/cv-loader/cv-loader.component.ts
+++ src/app/cv/cv-loader/cv-loader.component.ts
@@ -20,17 +20,23 @@ export class CvLoaderComponent {
     error = '';
 
     onFileSelected(event) {
-        const fileReader = new FileReader();
         const file = event.target.files[0];
+        const fileReader = new FileReader();
 
         fileReader.onload = () => {
-            this.error = '';
-            this.router.navigate(['/cv']);
-            this.cvService.setCv(JSON.parse(<string>fileReader.result));
+            try {
+                const data: string = JSON.parse(<string>fileReader.result);
+
+                this.error = '';
+                this.router.navigate(['/cv']);
+                this.cvService.setCv(data);
+            } catch (e) {
+                this.error = 'Please, choose a valid JSON file.';
+            }
         };
 
         fileReader.onerror = () => {
-            this.error = 'Error';
+            this.error = fileReader.error.message;
         };
 
         fileReader.readAsText(file);
