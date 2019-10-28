5bde8a6f9af1a3ceaa9e77be6f91cc1c7556d94b
Rafael Bicalho <rbmbika@gmail.com>
2019-10-28 13:53:13 -0300

    Add button to return to homepage from the cv page

--- src/app/cv/cv.module.ts
+++ src/app/cv/cv.module.ts
@@ -1,19 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
-import { CvComponent } from './cv.component';
-import { CvLoaderComponent } from './cv-loader/cv-loader.component';
+import { CvComponent } from "./cv.component";
+import { CvLoaderComponent } from "./cv-loader/cv-loader.component";
 
 @NgModule({
-    imports: [
-        CommonModule
-    ],
-    exports: [
-        CvComponent
-    ],
-    declarations: [
-        CvComponent,
-        CvLoaderComponent
-    ]
+    imports: [CommonModule, RouterModule],
+    exports: [CvComponent],
+    declarations: [CvComponent, CvLoaderComponent]
 })
 export class CvModule {}
