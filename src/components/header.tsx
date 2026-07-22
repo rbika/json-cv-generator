7805ccf7f30cd06e98465ca10eb488f988a31783
Rafael Bicalho <rbmbika@gmail.com>
2026-07-22 18:19:33 -0300

    feat: add link to project's Github

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,16 +3,29 @@ import Link from 'next/link'
 import { BracesIcon } from 'lucide-react'
 
 import { Container } from '@/components/container'
+import { GithubIcon } from '@/components/icons/github-icon'
 import { paths } from '@/utils/paths'
 
 const Header = () => {
   return (
     <nav className="bg-background fixed top-0 right-0 left-0 z-20 h-12 w-full border-b">
       <Container className="flex h-full items-center">
-        <Link href={paths.home()} className="flex items-center gap-2">
-          <BracesIcon className="size-6" />
-          <span className="text-xl font-bold">CV Generator</span>
-        </Link>
+        <div className="flex w-full items-center justify-between gap-2">
+          <Link href={paths.home()} className="flex items-center gap-2">
+            <BracesIcon className="size-6" />
+            <span className="text-xl font-bold">CV Generator</span>
+          </Link>
+
+          <div className="flex items-center gap-2">
+            <Link
+              href="https://github.com/rbika/json-cv-generator"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GithubIcon className="size-5" />
+            </Link>
+          </div>
+        </div>
       </Container>
     </nav>
   )
